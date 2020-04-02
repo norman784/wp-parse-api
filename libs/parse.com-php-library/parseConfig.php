11c855e334b96436e9f394ff9cdf592d00aeb446
Norman Paniagua <normanpaniagua@gmail.com>
2020-04-02 17:46:21 +0200

    The app url must end with a slash, if not present we add it.

--- libs/parse.com-php-library/parseConfig.php
+++ libs/parse.com-php-library/parseConfig.php
@@ -2,7 +2,11 @@
 if (!defined('WP_PARSE_API_APP_ID'))		define('WP_PARSE_API_APP_ID'		, get_option('app_id'));
 if (!defined('WP_PARSE_API_MASTERKEY'))		define('WP_PARSE_API_MASTERKEY'		, get_option('app_masterkey'));
 if (!defined('WP_PARSE_API_APP_RESTKEY'))	define('WP_PARSE_API_APP_RESTKEY'	, get_option('app_restkey'));
-if (!defined('WP_PARSE_API_APP_URL'))	define('WP_PARSE_API_APP_URL'	, get_option('app_url'));
+if (!defined('WP_PARSE_API_APP_URL'))	{
+  $app_url = get_option('app_url');
+  if (isset($app_url) && substr($app_url, -1) != '/') $app_url .= "/";
+  define('WP_PARSE_API_APP_URL'	, $app_url);
+}
 if (!defined('WP_PARSE_API_OBJECT_NAME'))	define('WP_PARSE_API_OBJECT_NAME'	, get_option('object_name') == "" ? 'Post' : get_option('object_name'));
 
 class parseConfig {
